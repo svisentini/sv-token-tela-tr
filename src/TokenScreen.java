1f18c77683988f7fb583d795a7dde4216ad08f33
Sidao <svisentini@gmail.com>
2024-05-20 22:29:05 -0300

    Creating Clear button

--- src/TokenScreen.java
+++ src/TokenScreen.java
@@ -7,7 +7,7 @@ public class TokenScreen {
         public static void main(String[] args) {
 // Create frame with title Registration Demo
                 JFrame frame = new JFrame();
-                frame.setTitle("Access token generator");
+                frame.setTitle("Access token generator - International - V2.0");
                 JPanel mainPanel = new JPanel();
                 mainPanel.setLayout(new BoxLayout(mainPanel, BoxLayout.Y_AXIS));
                 JPanel panel = new JPanel(new GridBagLayout());
@@ -29,7 +29,9 @@ public class TokenScreen {
                 JComboBox<String> environmentComboBox = new JComboBox<>(environmentOptions);
                 String[] systemOptions = {"ImportInt           "};
                 JComboBox<String> systemComboBox = new JComboBox<>(systemOptions);
-                JTextArea tokenArea = new JTextArea(2, 80);
+                JTextArea tokenArea = new JTextArea(5, 20);
+                tokenArea.setLineWrap(true);  // Habilita a quebra de linha
+                tokenArea.setWrapStyleWord(true);  // Habilita a quebra de palavras
 
                 // Default values
                 nameInput.setText("0180500");
@@ -37,55 +39,62 @@ public class TokenScreen {
                 environmentComboBox.setSelectedIndex(0); // Define a seleção inicial
                 systemComboBox.setSelectedIndex(0);
 
+                Integer coluna = 0;
                 // Username
-                constr.gridx = 0;
+                constr.gridx = coluna;
                 constr.gridy = 0;
                 panel.add(nameLabel, constr);
-                constr.gridx = 1;
+                constr.gridx = coluna + 1;
                 panel.add(nameInput, constr);
                 // Password
-                constr.gridx = 0;
+                constr.gridx = coluna;
                 constr.gridy = 1;
                 panel.add(passwordLabel, constr);
-                constr.gridx = 1;
+                constr.gridx = coluna + 1;
                 panel.add(passwordInput, constr);
                 // Environment
-                constr.gridx = 0;
+                constr.gridx = coluna;
                 constr.gridy = 2;
                 panel.add(environmentLabel, constr);
-                constr.gridx = 1;
+                constr.gridx = coluna + 1;
                 panel.add(environmentComboBox, constr);
                 // System
-                constr.gridx = 0;
+                constr.gridx = coluna;
                 constr.gridy = 3;
                 panel.add(systemLabel, constr);
-                constr.gridx = 1;
+                constr.gridx = coluna + 1;
                 panel.add(systemComboBox, constr);
                 // Token
-                constr.gridx = 0;
+                constr.gridx = coluna;
                 constr.gridy = 4;
                 panel.add(tokenLabel, constr);
-                constr.gridx = 1;
+                constr.gridx = coluna + 1;
                 panel.add(tokenArea, constr);
 
 //                constr.gridwidth = 2;
 //                constr.anchor = GridBagConstraints.CENTER;
                 // Buttons
-                JButton buttonGenerate = new JButton("Generate");
-                JButton buttonCopyClipboard = new JButton("Copy Clipboard");
-                JButton buttonExit = new JButton("Exit");
+                JButton buttonGenerate = new JButton(" Generate ");
+                JButton buttonClear = new JButton(" Clear ");
+                JButton buttonCopyClipboard = new JButton(" Copy ");
+                JButton buttonExit = new JButton(" Exit ");
 
                 // Button listeners
                 buttonGenerate.addActionListener(new ActionListener() {
                         public void actionPerformed(ActionEvent e) {
 
-                                try {
-                                        tokenArea.setText("Processing .....");
-                                        tokenArea.setText(Actions.actionGenerate(nameInput, passwordInput, systemComboBox, environmentComboBox, tokenArea));
-                                } catch (IOException ex) {
-                                        tokenArea.setText("Error processing token");
-                                        throw new RuntimeException(ex);
-                                }
+                                SwingUtilities.invokeLater(() -> tokenArea.setText("Processing ....."));
+
+                                new Thread(() -> {
+                                        try {
+                                                tokenArea.setText(Actions.actionGenerate(nameInput, passwordInput, systemComboBox, environmentComboBox, tokenArea));
+                                        } catch (IOException ex) {
+                                                tokenArea.setText("Error processing token");
+                                                throw new RuntimeException(ex);
+                                        }
+                                }).start();
+
+
                         }
                 });
                 buttonCopyClipboard.addActionListener(new ActionListener() {
@@ -98,21 +107,30 @@ public class TokenScreen {
                                 Actions.actionExit(frame);
                         }
                 });
+                buttonClear.addActionListener(new ActionListener() {
+                        @Override
+                        public void actionPerformed(ActionEvent actionEvent) {
+                                tokenArea.setText("");
+                        }
+                });
 
                 constr.gridx = 0;
-                constr.gridy = 5;
+                constr.gridy = 6;
                 panel.add(buttonGenerate, constr);
-                constr.gridx = 1;
-                constr.gridy = 5;
-                panel.add(buttonCopyClipboard, constr);
                 constr.gridx = 2;
-                constr.gridy = 5;
+                constr.gridy = 6;
+                panel.add(buttonClear, constr);
+                constr.gridx = 1;
+                constr.gridy = 6;
+                panel.add(buttonCopyClipboard, constr);
+                constr.gridx = 3;
+                constr.gridy = 6;
                 panel.add(buttonExit, constr);
 //                mainPanel.add(headingPanel);
                 mainPanel.add(panel);
                 frame.add(mainPanel);
                 frame.pack();
-                frame.setSize(1200, 270);
+                frame.setSize(600, 350);
                 frame.setLocationRelativeTo(null);
                 frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
                 frame.setVisible(true);
